65802c118b2c62df2ff03d76ef86ddb0865c5540
Mikko Haikonen <mikko.haikonen@mediamaisteri.com>
2017-06-01 12:36:24 +0300

    Translated comments to English

--- block_mmquicklink.php
+++ block_mmquicklink.php
@@ -21,13 +21,8 @@
  */
 
 /************************
-Admineille, hallinnoijille ja opettajille näkyvä
-lohko, jossa on pikalinkkejä näppäriin toimenpiteisiin.
-Linkit vaihtuvat dynaamisesti sivun mukaan, eli kursseilla
-on eri toimintoja, kuin etusivulla.
-
-Moduulin "Missä tämä lohko näkyy" -asetus tulee olla "Mikä tahansa sivu",
-jotta lohko toimii halutulla tavalla.
+Quick Link -block to help admins, managers
+and teacher to navigate through Moodle.
 
 2017
 Mediamaisteri Oy
@@ -52,7 +47,7 @@ user_has_role_assignment($USER->id, 2, context_system::instance()->id)) {
         public function html_attributes() {
             $attributes = parent::html_attributes();
             if ($this->instance_can_be_docked() && get_user_preferences('docked_block_instance_'.$this->instance->id, 1)) {
-                // Telakoidaan lohko defaulttina. Jos käyttäjä muuttaa telakointia, muutokset overridaa tämän.
+                // Dock block on default.
                 $attributes['class'] .= ' dock_on_load';
             }
             return $attributes;
@@ -70,11 +65,11 @@ user_has_role_assignment($USER->id, 2, context_system::instance()->id)) {
             global $PAGE, $CFG, $USER, $COURSE; // Load required globals.
             $this->content->text = ""; // Set variable.
 
-            // Tämän alle linkit, jotka löytyvät kurssisivulta.
+            // Links to show on course pages.
             if ($PAGE->pagelayout == 'course' || $PAGE->pagelayout == 'incourse' || $PAGE->pagelayout == 'report' ||
             $PAGE->pagetype == 'course-view-topics') {
 
-                // Muokkaustila päälle tai pois -painike. Ensin tarkistetaan onko oikeuksia asiaan.
+                // Editing mode on/off link.
                 if (has_capability('moodle/course:update', context_system::instance())) {
                     if ($PAGE->user_is_editing()) {
                         $editingmode = "off";
@@ -88,7 +83,7 @@ user_has_role_assignment($USER->id, 2, context_system::instance()->id)) {
                         "&sesskey=" . $USER->sesskey) . "'>" . $editingmodestring . "</a>";
                 }
 
-                // Piilota/Näytä kurssi -painike.
+                // Show/hide course link.
                 if (has_capability('moodle/course:visibility', context_system::instance())) {
                     if ($COURSE->visible == "1") {
                         $this->content->text .= "<li class='list'><a href='" .
@@ -103,12 +98,12 @@ user_has_role_assignment($USER->id, 2, context_system::instance()->id)) {
                     }
                 }
 
-                // Lisää "completion progress" -lohko.
+                // Add a "completion progress" block.
                 if (has_capability('block/completion_progress:addinstance', context_system::instance())) {
                         $this->content->text .= "<li class='list'><a href='" . $CFG->wwwroot . "/course/view.php?id=" . $COURSE->id . "&sesskey=" . $USER->sesskey . "&bui_addblock=completion_progress'>" . get_string('add') . " " . strtolower(get_string('pluginname', 'block_completion_progress')) . "</a></li>";
                 }
 
-                // Näytetään kurssiavaimenluontipainike, jos kurssinmuokkaukseen on oikeus.
+                // Show enrolment key add button.
                 if (has_capability('moodle/course:update', context_system::instance())) {
                     $this->content->text .= "<li class='list mmquicklink-enrolmentkey'><a href=''>" . get_string('set', 'portfolio_flickr') . " " .
                         strtolower(get_string('password', 'enrol_self')) . "</a></li>
@@ -118,22 +113,22 @@ user_has_role_assignment($USER->id, 2, context_system::instance()->id)) {
                             <input type='text' name='enrolmentkey'> <input type='submit' value='" . get_string('save', 'core_admin') . "'>
                         </div>";
                 }
-                // Osallistujat -sivu.
+                // Course participants.
                 if (has_capability('moodle/course:viewparticipants', context_system::instance())) {
                     $this->content->text .= "<li class='list'><a href='" . new moodle_url($CFG->wwwroot .
                     "/user/index.php?id=" . $PAGE->course->id) . "'>" .
                         get_string('show') . " " . strtolower(get_string('participants')) . "</a></li>";
                 }
-                // Kurssin arvioinnit -sivut.
+                // Course grading.
                 if (has_capability('mod/assign:grade', context_system::instance())) {
                     $this->content->text .= "<li class='list'><a href='" . new moodle_url($CFG->wwwroot .
                     "/grade/report/grader/index.php?id=" . $PAGE->course->id) . "'>" . get_string('coursegrades') . "</a></li>";
                 }
 
             } else {
-                // Tähän alle linkit, jotka löytyvät muilta sivuilta.
+                // Link on otherp ages.
 
-                // Muokkaustila päälle tai pois -painike. Ensin tarkistetaan onko oikeuksia asiaan.
+                // Editing mode on/off link.
                 if (is_siteadmin() OR user_has_role_assignment($USER->id, 1, context_system::instance()->id)) {
                     if ($PAGE->user_is_editing()) {
                         $editingmode = "off";
@@ -156,19 +151,20 @@ user_has_role_assignment($USER->id, 2, context_system::instance()->id)) {
 
                 }
 
-                // Näytetään kurssinlisäyspainike, jos siihen on oikeuksia.
+                // show "add a course" button.
                 if (has_capability('moodle/course:create', context_system::instance())) {
                     $this->content->text .= "<li class='list'><a href='" .
                         new moodle_url($CFG->wwwroot . "/course/edit.php?category=1") . "'>".
                         get_string('addnewcourse') . "</a></li>";
                 }
-                // Näytetään kurssienhallintalinkki, jos sellaiseen on oikeudet.
+
+                // Show course management button.
                 if (has_capability('moodle/category:manage', context_system::instance())) {
                     $this->content->text .= "<li class='list'><a href='" .
                         new moodle_url($CFG->wwwroot . "/course/management.php") . "'>".
                         get_string('coursecatmanagement') . "</a></li>";
                 }
-                // Etusivun asetukset -linkki näytetään etusivulla kaikille, joille lohko näkyy.
+                // Frontpage settings link.
                 if (is_siteadmin() OR user_has_role_assignment($USER->id, 1, context_system::instance()->id)) {
                     if($PAGE->pagelayout == 'frontpage') {
                         $this->content->text .= "<li class='list'><a href='" .
