50ca5e7553b6cc232c0ab6983e270fd14104d82a
Mikko Haikonen <mikko.haikonen@mediamaisteri.com>
2019-11-26 11:46:41 +0200

    Release & version bump

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_mmquicklink';
-$plugin->version = 2019112600;
+$plugin->version = 2019112610;
 $plugin->requires = 2016052300;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.1.1";
+$plugin->release = "1.1.2";
