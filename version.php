0d56484fc057f2d6ee39ac3e27d67bed836170cf
Mikko Haikonen <mikko.haikonen@iki.fi>
2020-06-03 11:20:56 +0300

    mReports nav fix

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_mmquicklink';
-$plugin->version = 2020052500;
+$plugin->version = 2020060300;
 $plugin->requires = 2016052300;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.4.11";
+$plugin->release = "1.4.12";
