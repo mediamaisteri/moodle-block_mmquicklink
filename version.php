0a3e90bb71a6bcefff56b59c91ab4d058a14ec4f
Rosa Siuruainen <rosa.siuruainen@mediamaisteri.com>
2022-02-28 12:24:51 +0200

    Add link to competence report under mReports

--- version.php
+++ version.php
@@ -29,4 +29,4 @@ $plugin->component = 'block_mmquicklink';
 $plugin->version = 2021101900;
 $plugin->requires = 2016052300;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.10.1";
+$plugin->release = "1.10.2";
